88954328fe6032a1647387578f4d7b472ec1acdf
Andrei Galkin <andron.galkin@gmail.com>
2018-03-20 06:54:51 +0600

    Bubble Sort

---
+++ Lib/Algorithms/BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿namespace Algorithms
+{
+    public class BubbleSort
+    {
+        /// <summary>
+        /// Bubble Sort
+        /// </summary>
+        public static void Sort(int[] array)
+        {
+            Sort(array, array.Length - 2);
+        }
+
+        private static void Sort(int[] array, int size)
+        {
+            if (size == 1) return;
+
+            for (int j = 0; j <= array.Length - 2; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                   Swap(array, j, j + 1);
+                }
+            }
+            Sort(array, size - 1);
+        }
+
+        private static void Swap(int[] array, int left, int rigt)
+        {
+            int temp = array[left];
+
+            array[left] = array[rigt];
+            array[rigt] = temp;
+        }
+    }
+}
