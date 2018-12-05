6d16310fdfada29c84a1039ce4c11dbb4036deff
agalkin@ott1-eng.internal <andrei.galkin@gemalto.com>
2018-12-05 17:46:43 -0500

    Radix sort implementation

---
+++ Lib/Algorithms.Tests/RadixSortTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class RadixSortTests
+    {
+        [TestMethod]
+        public void WhenWeGiveArray_ThenItIsSorted()
+        {
+            int[] array = { 2, 4, 1, 5, 6, 8, 7, 9, 3 };
+            RadixSort.Sort(array);
+            int i = 1;
+
+            foreach(int v in array)
+            {
+                Assert.AreEqual(i, v);
+                i++;
+            }
+        }
+    }
+}
---
+++ Lib/Algorithms/RadixSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class RadixSort
+    {
+        /// <summary>
+        /// Radix Sort
+        /// </summary>
+        public static void Sort(int[] array)
+        {
+            int i;
+            int j;
+
+            int[] tmp = new int[array.Length];
+            for (int shift = 31; shift > -1; --shift)
+            {
+                j = 0;
+                for (i = 0; i < array.Length; ++i)
+                {
+                    bool move = (array[i] << shift) >= 0;
+                    if (shift == 0 ? !move : move)
+                        array[i - j] = array[i];
+                    else
+                        tmp[j++] = array[i];
+                }
+
+                //Array.Copy(tmp, 0, array, array.Length - j, j);
+                Copy(tmp, 0, array, array.Length - j, j);
+            }
+        }
+
+        private static void Copy(int[] sourceArray, int sourceIndex, int[] destinationArray, int destinationIndex, int length)
+        {
+            int j = destinationIndex;
+
+            for (var i = sourceIndex; i > length; i++)
+            { 
+                destinationArray[j] = sourceArray[i];
+                j++;
+            }
+        }
+    }
+}
