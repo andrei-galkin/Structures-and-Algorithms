78bdeb158f5cd3be0477b5379d65134ebcaa38d1
agalkin@ott1-eng.internal <andrei.galkin@gemalto.com>
2021-02-06 18:02:42 -0500

    Update

--- Lib/Algorithms/QuickSort.cs
+++ Lib/Algorithms/QuickSort.cs
@@ -1,4 +1,9 @@
-﻿namespace Algorithms
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace Algorithms
 {
     public class QuickSort
     {
@@ -7,15 +12,19 @@
         /// </summary>
         public static void Sort(int[] array)
         {
-            Sort(array, 0, array.Length - 1);
+            SortI(array, 0, array.Length - 1);
         }
 
+        
         private static void Sort(int[] array, int leftIndex, int rightIndex)
         {
-            if (leftIndex >= rightIndex) return;
+            if (leftIndex >= rightIndex)
+                return;
 
             int pivotElement = array[(rightIndex + leftIndex) / 2];
+
             int partitionIndex = Partition(array, pivotElement, leftIndex, rightIndex);
+
             Sort(array, leftIndex, partitionIndex - 1);
             Sort(array, partitionIndex, rightIndex);
         }
@@ -45,7 +54,47 @@
             return leftIndex;
         }
 
-        private static void Swap(int[] array, int left, int rigt)
+
+        public static void SortI(int[] array, int leftIndex, int rightIndex)
+        {
+            if (leftIndex >= rightIndex) return;
+
+            var partitionElement = array[(leftIndex + rightIndex) / 2];
+
+            int partitionIndex = Partitioning(array, partitionElement, leftIndex, rightIndex);
+
+            SortI(array, leftIndex, partitionIndex - 1);
+
+            SortI(array, partitionIndex, rightIndex);
+            
+        }
+
+        private static int Partitioning(int[] array, int partitionElement, int startIndex, int endIndex)
+        {
+            while (startIndex <= endIndex)
+            {
+                while (array[startIndex] < partitionElement)
+                {
+                    startIndex++;
+                }
+
+                while (array[endIndex] > partitionElement)
+                {
+                    endIndex--;
+                }
+
+                if (startIndex <= endIndex)
+                {
+                    Swap(array, startIndex, endIndex);
+                    startIndex++;
+                    endIndex--;
+                }
+            }
+
+            return startIndex;
+        }
+
+    private static void Swap(int[] array, int left, int rigt)
         {
             int temp = array[left];
 
