071180e4d4730df8f0ad12eeacec024a30bc7a02
agalkin@ott1-eng.internal <andrei.galkin@gemalto.com>
2021-03-26 10:42:43 -0400

    Dijkstra the first iteration

--- Lib/Algorithms.Tests/MergeSortTest.cs
+++ Lib/Algorithms.Tests/MergeSortTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Algorithms.Tests
 {
@@ -17,6 +20,61 @@ namespace Algorithms.Tests
                 Assert.AreEqual(i, v);
                 i++;
             }
+        }
+
+        [TestMethod]
+        public void test()
+        {
+            var arr = new int[] {1, 3, 6, 7, 9, 11, 15, 17};
+            var r = twoSum(arr, 17);
+            var f = Fib(7);
+
+            Assert.AreEqual(r, true);
+        }
+
+        private bool twoSum(int[] array, int target)
+        {
+            int leftIndex = 0;
+            int rightIndex = 0;
+
+            while (leftIndex <= rightIndex)
+            {
+                if (array[leftIndex] + array[rightIndex] == target)
+                {
+                    Console.WriteLine(array[leftIndex] + " " + array[rightIndex]);
+                    return true;
+                }
+
+                if (array[leftIndex] + array[rightIndex] < target)
+                {
+                    leftIndex++;
+                }
+                else
+                {
+                    rightIndex--;
+                }
+            }
+
+            return true;
+        }
+
+        private int Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+
+            var previousFib = 0;
+            var currentFib = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                // loop is skipped if n = 1
+                var newFib = previousFib + currentFib;
+                previousFib = currentFib;
+                currentFib = newFib;
+            }
+
+            return currentFib;
         }
     }
 }
